de9121edfb63c2d87c7284a1ab316b4c9a0c566f
TimeLance89 <80360241+TimeLance89@users.noreply.github.com>
2026-07-31 12:24:09 +0200

    Projektstand aktualisieren

--- server.py
+++ server.py
@@ -11774,7 +11774,9 @@ class NoCacheStaticFiles(StaticFiles):
 
     async def get_response(self, path, scope):
         response = await super().get_response(path, scope)
-        response.headers["Cache-Control"] = "no-cache, must-revalidate"
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
         return response
 
 
